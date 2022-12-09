d50f44fb1063009eecd4c06e57928680de37bba2
Suyog <suyogchaudhari57@gmail.com>
2022-12-09 23:23:12 +0530

    Updates

---
+++ precache-manifest.4319909d0d8d09a122771e6b6e075476.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "85f6a6baa6710b53b80e",
+    "url": "/static/css/main.64ea83ef.chunk.css"
+  },
+  {
+    "revision": "85f6a6baa6710b53b80e",
+    "url": "/static/js/main.85f6a6ba.chunk.js"
+  },
+  {
+    "revision": "5a63e80967d755f43234",
+    "url": "/static/js/1.5a63e809.chunk.js"
+  },
+  {
+    "revision": "2de54c7c198847dd935d",
+    "url": "/static/css/2.f08c9a64.chunk.css"
+  },
+  {
+    "revision": "2de54c7c198847dd935d",
+    "url": "/static/js/2.2de54c7c.chunk.js"
+  },
+  {
+    "revision": "55222b96e37d19236616",
+    "url": "/static/js/runtime~main.55222b96.js"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "4acccf5c75dc399dd24840f917426dde",
+    "url": "/index.html"
+  }
+];
